4714633d4c125365a7963350656b57e45058666c
Maximesol <maxime.gognies@gmail.com>
2023-11-23 20:03:40 +0100

    ✨ contracts > add TokenGouv.sol

---
+++ backend/contracts/TokenGouv.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract TokenGouv is ERC20, Ownable, ERC20Permit, ERC20Votes {
+    //uint256 public s_maxSupply = 1000000 * 10**18;
+
+    constructor(address initialOwner)
+        ERC20("TokenGouv", "DCP")
+        Ownable(initialOwner)
+        ERC20Permit("TokenGouv")
+    {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner)
+        public
+        view
+        override(ERC20Permit, Nonces)
+        returns (uint256)
+    {
+        return super.nonces(owner);
+    }
+}
