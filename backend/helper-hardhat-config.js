e6f663c07f7c800dacc9f59936352aa31b3709d7
Maximesol <maxime.gognies@gmail.com>
2023-11-24 15:47:43 +0100

    🔧 backend > add helper-hardhat-config.js

---
+++ backend/helper-hardhat-config.js
@@ -0,0 +1,10 @@
+export const QUORUM_PERCENTAGE = 7 // Need 7% of voters to pass
+export const MIN_DELAY = 3600 // 1 hour - after a vote passes, you have 1 hour before you can enact
+// export const VOTING_PERIOD = 45818 // 1 week - how long the vote lasts. This is pretty long even for local tests
+export const VOTING_PERIOD = 3 // blocks
+export const VOTING_DELAY = 0 // 1 Block - How many blocks till a proposal vote becomes active
+export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000"
+
+export const NEW_STORE_VALUE = 77
+export const FUNC = "store"
+export const PROPOSAL_DESCRIPTION = "Proposal #1 77 in the Box!"
