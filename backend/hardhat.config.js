9d1443ce1dd8c983d3b8766b5ab370e76d5c6a3b
Maximesol <maxime.gognies@gmail.com>
2023-11-24 17:27:48 +0100

    🔧 hardhat-config > add allowUnlimitedContractSize and others config for the project

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,43 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv/config");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+require("@nomicfoundation/hardhat-verify");
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.20",
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 11155111,
+      blockConfirmations: 6,
+    },
+    hardhat: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true
+    },
+    localhost: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true
+    },
+
+
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
