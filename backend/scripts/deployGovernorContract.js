dd2041ff46bbe2e9cdffde14db062f0c5c76baed
Maximesol <maxime.gognies@gmail.com>
2023-11-24 17:28:20 +0100

    ✨ backend > scripts > add deployGovernorContract.js

---
+++ backend/scripts/deployGovernorContract.js
@@ -0,0 +1,37 @@
+const hre = require("hardhat");
+const fs = require('fs');
+const {QUORUM_PERCENTAGE, MIN_DELAY, VOTING_PERIOD, VOTING_DELAY, ADDRESS_ZERO} = require("../helper-hardhat-config");
+
+async function deployGovernorContract() {
+  const [owner, addr1, addr2] = await hre.ethers.getSigners();
+    console.log(
+    "Deploying contracts with the account:",
+    owner.address
+    );
+
+  // Charger les adresses déployées
+  const { tokenGouvAddress, timeLockAddress } = JSON.parse(fs.readFileSync('deployedAddresses.json', 'utf8'));
+  console.log(`TokenGouv Address: ${tokenGouvAddress}`);
+  console.log(`TimeLock Address: ${timeLockAddress}`);
+
+    // Deploy GovernorContract
+  const GovernorContract = await hre.ethers.getContractFactory("GovernorContract");
+  const governorContract = await GovernorContract.deploy(
+    tokenGouvAddress,
+    timeLockAddress,
+    VOTING_DELAY,
+    VOTING_PERIOD,
+    QUORUM_PERCENTAGE,
+  );
+  await governorContract.waitForDeployment();
+
+  console.log(`GovernorContract deployed to ${governorContract.target}`);
+
+}
+
+deployGovernorContract()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
