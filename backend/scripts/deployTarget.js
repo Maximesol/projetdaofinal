f51bbda01cdd00eaff111bebbc9213c97e0a3c4c
Maximesol <maxime.gognies@gmail.com>
2023-11-25 20:46:42 +0100

    ♻️ scripts > refacto of deployTarget

--- backend/scripts/deployTarget.js
+++ backend/scripts/deployTarget.js
@@ -6,10 +6,10 @@ async function deployTarget() {
     console.log("Deploying contracts with the account:", owner.address);
 
   // Charger les adresses déployées
-  const {timeLockAddress } = JSON.parse(fs.readFileSync('deployedAddresses.json', 'utf8'));
-  console.log(`TimeLock Address: ${timeLockAddress}`);
+  const addresses = JSON.parse(fs.readFileSync('deployedAddresses.json', 'utf8'));
+  console.log(`TimeLock Address: ${addresses.timeLockAddress}`);
 
-    // Deploy GovernorContract
+    // Deploy TargetContract
   const TargetContract = await hre.ethers.getContractFactory("TargetContract");
   const targetContract = await TargetContract.deploy(owner)
   await targetContract.waitForDeployment();
@@ -17,19 +17,23 @@ async function deployTarget() {
   console.log(`target deployed to ${targetContract.target}`);
 
   const target = await ethers.getContractAt("TargetContract", targetContract.target)
-  const transferTx = await target.transferOwnership(timeLockAddress)
+  const transferTx = await target.transferOwnership(addresses.timeLockAddress)
   await transferTx.wait(1)
 
   // Vérifier le nouveau propriétaire
   const newOwner = await targetContract.owner();
   console.log(`New owner of TargetContract: ${newOwner}`);
 
+  addresses.targetContractAddress = targetContract.target;
+  fs.writeFileSync('deployedAddresses.json', JSON.stringify(addresses, null, 2));
+
   // Vérification
-  if (newOwner === timeLockAddress) {
+  if (newOwner === addresses.timeLockAddress) {
     console.log("Ownership successfully transferred to TimeLock.");
   } else {
     console.log("Ownership transfer failed.");
   }
+
   
 }
 
