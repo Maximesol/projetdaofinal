9a16148c0523c31c10bed8c69489a9c7fe45964d
Maximesol <maxime.gognies@gmail.com>
2023-12-02 14:58:55 +0100

    🚧 fronted > YourInfoContent > add layout with address connected

--- frontend/app/components/YourInfoContent.jsx
+++ frontend/app/components/YourInfoContent.jsx
@@ -1,7 +1,27 @@
+'use client'
 import React from 'react'
+import { Box, Text } from '@chakra-ui/react';
+import { useAccount } from "wagmi";
+
+
 
 export default function YourInfoContent() {
+  const { address, isConnected } = useAccount();
+
+
+  const formatAddress = (address) => {
+    // Affiche les 6 premiers caractères, suivi de '...', suivi des 4 derniers caractères
+    return `${address.substring(0, 6)}...${address.substring(address.length - 4)}`;
+  };
+
+
   return (
-    <div>YourInfoContent</div>
+    <Box boxShadow="md">
+      {isConnected ? (
+        <Text>Adresse connectée : {formatAddress(address)}</Text>
+      ) : (
+        <Text>Aucun utilisateur connecté</Text>
+      )}
+    </Box>
   )
 }
