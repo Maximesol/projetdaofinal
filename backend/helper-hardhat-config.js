cd0155aa5b25b596c9c0297db8236a05fdd5ef3b
Maximesol <maxime.gognies@gmail.com>
2023-12-04 20:21:03 +0100

    ⚡️ backend > helper > change VOTING DELAY TO 1

--- backend/helper-hardhat-config.js
+++ backend/helper-hardhat-config.js
@@ -1,13 +1,13 @@
 const QUORUM_PERCENTAGE = 7; // Need 7% of voters to pass
 const MIN_DELAY = 3600; // 1 hour - after a vote passes, you have 1 hour before you can enact
 const VOTING_PERIOD = 3; // blocks
-const VOTING_DELAY = 0; // 1 Block - How many blocks till a proposal vote becomes active
+const VOTING_DELAY = 1; // 1 Block - How many blocks till a proposal vote becomes active
 const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000";
 
 const NEW_STORE_VALUE = 77;
 const FUNC = "store";
 const PROPOSAL_DESCRIPTION = "Proposal #1 77 in the Target Contract!";
-const PROPOSAL_DESCRIPTION_2 = "Proposal #2 Transfert 10 eth from token gouv contrat to Target Contract!"
+const PROPOSAL_DESCRIPTION_2 = "Transfert 10 eth from token gouv contrat to Target Contract!"
 const developmentChains = ["hardhat", "localhost"];
 const proposalsFiles = "proposals.json"
 
