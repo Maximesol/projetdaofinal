6ae1c99c39fa057265db515ae2d0e890b83af860
Maximesol <maxime.gognies@gmail.com>
2023-12-04 09:45:22 +0100

    🐛 frontend > Nav > fix the hydration bug with the Link

--- frontend/app/components/Nav.js
+++ frontend/app/components/Nav.js
@@ -1,21 +1,32 @@
+'use client'
 import React from 'react';
 import { Box, Flex, Link, Spacer } from '@chakra-ui/react';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
+import NextLink from 'next/link'
 
 export default function Navigation() {
   return (
+    <>
     <Flex as="nav" bg="gray.800" p="4" align="center" width="100vw" height="80px">
       <Box fontWeight="bold" fontSize="lg">
         DeFi Capital Partner
       </Box>
       <Spacer />
       <Box>
-        <Link href="/" mx="2">Home</Link>
-        <Link href="/governance" mx="2">Governance</Link>
-        <Link href="/forum" mx="2">Forum</Link>
+        {/* <Link href="/" mx="2">Home</Link> */}
+        <Link as={NextLink} href='/'>
+          Home
+        </Link>
+
+        <Link as={NextLink} href="/governance" mx="2">
+        Governance
+        </Link>
+
+        <Link as={NextLink} href="/forum" mx="2">Forum</Link>
       </Box>
       <Spacer />
       <ConnectButton />
     </Flex>
+    </>
   );
 }
