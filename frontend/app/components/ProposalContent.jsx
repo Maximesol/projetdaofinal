39edc1ba4aced852464b36d54627637d1c30be91
Maximesol <maxime.gognies@gmail.com>
2023-12-04 09:45:47 +0100

    ♻️ frontend > refacto ProposalContent

--- frontend/app/components/ProposalContent.jsx
+++ frontend/app/components/ProposalContent.jsx
@@ -1,16 +1,27 @@
-import React, { useContext } from 'react'
+'use client'
+import React, { useContext, useState, useEffect } from 'react';
 import { Box } from '@chakra-ui/react';
-import ContractContext from '../context/ContractContext';
+import { ContractContext } from '../context/GovernorContractProvider';
+import { useAccount } from "wagmi";
 
 export default function ProposalContent() {
-  const {numberOfProposals, isConnected} = useContext(ContractContext)
+  const { numberOfProposals } = useContext(ContractContext);
+  const { isConnected } = useAccount();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+
+  if (!isLoaded) {
+    return <Box>Chargement...</Box>;
+  }
 
   return (
     <Box>
-        {isConnected ? 
-        `Il y a actuellement ${numberOfProposals} propositions` : 
-        "Connectez vous pour voir les propositions"}
-        
+      {isConnected 
+        ? `Il y a actuellement ${numberOfProposals} propositions` 
+        : "Connectez-vous pour voir les propositions"}
     </Box>
-  )
+  );
 }
