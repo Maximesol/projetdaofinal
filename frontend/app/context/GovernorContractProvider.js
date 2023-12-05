65b64c364d813813f645b8907a640bb40006003d
Maximesol <maxime.gognies@gmail.com>
2023-12-05 19:47:01 +0100

    ⚡️ frontend > Context > Add good state for the proposals, add getState and logs for the event of proposal

--- frontend/app/context/GovernorContractProvider.js
+++ frontend/app/context/GovernorContractProvider.js
@@ -1,12 +1,8 @@
 'use client'
 import React, { createContext, useState, useEffect } from 'react';
-import { useContractEvent, useAccount } from "wagmi";
-
-import {
-  readContract,
-  getWalletClient,
-  waitForTransaction,
-} from "@wagmi/core";
+import { useContractEvent, useAccount, usePublicClient } from "wagmi";
+import { parseAbiItem } from "viem";
+import { readContract, getWalletClient, waitForTransaction } from "@wagmi/core";
 import { abiGovernorContract, contractAddressGovernorContract } from "../constants/constantGovernorContract"
 
 export const ContractContext = createContext();
@@ -15,6 +11,84 @@ export const GovernorContractProvider = ({ children }) => {
   const [numberOfProposals, setNumberOfProposals] = useState(0);
   const { isConnected } = useAccount();
   const [isLoading, setIsLoading] = useState(false); 
+  const [proposalEvents, setProposalEvents] = useState([]);
+  const [pastProposals, setPastProposals] = useState([]);
+  const [combinedProposals, setCombinedProposals] = useState([]);
+  const viemPublicClient = usePublicClient();
+
+
+  // combined proposals
+
+  useEffect(() => {
+    const allProposals = [
+      ...pastProposals.map(p => ({ ...p, proposalId: p.proposalId.toString() })), 
+      ...proposalEvents
+    ];
+    const uniqueProposals = allProposals.reduce((acc, proposal) => {
+      if (!acc.some(p => p.proposalId === proposal.proposalId)) {
+        acc.push(proposal);
+      }
+      return acc;
+    }, []);
+    setCombinedProposals(uniqueProposals);
+    console.log("Combined proposals updated:", uniqueProposals);
+  }, [pastProposals, proposalEvents]);
+
+
+  useEffect(() => {
+    console.log("Current pastProposals:", pastProposals);
+  }, [pastProposals]);
+
+  useEffect(() => {
+    console.log("Current proposalEvents:", proposalEvents);
+  }, [proposalEvents]);
+
+  useEffect(() => {
+    console.log("Current combinedProposals:", combinedProposals);
+  }, [combinedProposals]);
+
+
+  const getState = async (proposalId) => {
+    const walletClient = await getWalletClient();
+    try {
+      const data = await readContract({
+        client: walletClient,
+        address: contractAddressGovernorContract,
+        abi: abiGovernorContract,
+        functionName: 'state',
+        args: [proposalId],
+      })
+      console.log("State for proposalId", proposalId, "is", data);
+      return data; // Retourner l'état récupéré
+    } catch (error) {
+      console.error("Erreur lors de la récupération de l'état du contrat:", error);
+    }
+  };
+
+  //Get past proposal Ids
+  useEffect(() => {
+    console.log("Component mounted");
+    const getProposalIdsLogs = async () => {
+      const proposalIdsLogs = await viemPublicClient.getLogs({
+        address: contractAddressGovernorContract,
+        event: parseAbiItem("event ProposalCreated(uint256 proposalId, address proposer, address[] targets, uint256[] values, string[] signatures, bytes[] calldatas, uint256 voteStart, uint256 voteEnd, string description)"),
+        fromBlock: 0n,
+      });
+      const newPastProposals = [...pastProposals];
+      for (const log of proposalIdsLogs) {
+        const proposalId = log.args.proposalId;
+        const description = log.args.description;
+        const isDuplicate = newPastProposals.some((proposal) => proposal.proposalId === proposalId);
+
+        if (!isDuplicate) {
+          newPastProposals.push({ proposalId, description });
+        }
+      }
+      setPastProposals(newPastProposals);
+      console.log("Past proposals after update:", pastProposals);
+    };
+    getProposalIdsLogs();
+  }, [isConnected]);
 
   const getNumberOfProposals = async () => { 
     const walletClient = await getWalletClient();
@@ -45,11 +119,16 @@ export const GovernorContractProvider = ({ children }) => {
     abi: abiGovernorContract,
     eventName: "ProposalCreated",
     listener: (events) => {
-      for (const event of events) {
-        const { proposalId, description } = event.args;
-        console.log(`Nouvelle proposition enregistrée : ${description}, ID: ${proposalId}`);
-      }
+      // for (const event of events) {
+      //   const { proposalId, description } = event.args;
+      //   console.log(`Nouvelle proposition enregistrée : ${description}, ID: ${proposalId}`);
+      // }
+      const newEvents = events.map(event => ({
+        proposalId: event.args.proposalId.toString(),
+        description: event.args.description
+      }));
       getNumberOfProposals();
+      setProposalEvents(prevEvents => [...prevEvents, ...newEvents]);
     },
   });
 
@@ -63,6 +142,9 @@ export const GovernorContractProvider = ({ children }) => {
     numberOfProposals,
     isLoading,
     getNumberOfProposals,
+    proposalEvents,
+    getState,
+    combinedProposals,
 
   };
 
