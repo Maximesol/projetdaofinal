1b19450037edafc6dc8ee77b57d21a8abe29de60
Maximesol <maxime.gognies@gmail.com>
2023-12-05 19:46:01 +0100

    💄 frontend > Add Proposals layout

--- frontend/app/components/Proposals.jsx
+++ frontend/app/components/Proposals.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+'use client'
+import React, { useContext, useEffect, useState } from 'react';
+import ProposalDetails from './ProposalDetails';
+import { ContractContext } from '../context/GovernorContractProvider';
 
-export default function Proposals() {
+
+const Proposals = () => {
+  const { combinedProposals } = useContext(ContractContext);  
+
+  console.log("combinedProposals : ", combinedProposals);
+
   return (
-    <div>Proposals</div>
-  )
-}
+    <div>
+      {combinedProposals.map(event => (
+        <ProposalDetails key={event.proposalId} proposalId={event.proposalId} />
+      ))}
+    </div>
+  );
+};
+
+export default Proposals;
