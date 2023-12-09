e9b6646fd54451c704b657c350c8fd7ad5baf2d4
Maximesol <maxime.gognies@gmail.com>
2023-12-09 18:41:06 +0100

    💄 frontend > Proposals > add message if empty proposal

--- frontend/app/components/Proposals.jsx
+++ frontend/app/components/Proposals.jsx
@@ -1,17 +1,29 @@
 'use client'
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import ProposalDetails from './ProposalDetails';
 import { ContractContext } from '../context/GovernorContractProvider';
-
+import { Box, Text } from '@chakra-ui/react';
 
 const Proposals = () => {
   const { combinedProposals } = useContext(ContractContext);  
 
+  // Vérifie si la liste des propositions est vide
+  if (combinedProposals.length === 0) {
+    // Affiche un message si aucune proposition n'est disponible
+    return (
+      <Box textAlign="center" p={4}>
+        <Text fontSize="lg" fontWeight="bold" color="gray.300">
+          Aucune proposition disponible pour le moment.
+        </Text>
+      </Box>
+    );
+  }
 
+  // Sinon, affiche la liste des propositions
   return (
     <div>
       {combinedProposals.map(event => (
-        <ProposalDetails key={event.proposalId} proposalId={event.proposalId} description={event.description}/>
+        <ProposalDetails key={event.proposalId} proposalId={event.proposalId} description={event.description} />
       ))}
     </div>
   );
